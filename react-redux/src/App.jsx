b373807a8bc5c27cb2020770b5fedb2ccfed2da0
Shivam-136 <shivam243028@gmail.com>
2026-05-08 22:47:25 +0530

    react-redux-syntex

--- react-redux/src/App.jsx
+++ react-redux/src/App.jsx
@@ -12,20 +12,22 @@ function App() {
 
   return (
     <div
-      style={{ display: "flex",gap: "20px",alignItems: "center",}}>
+      style={{ display: "flex", gap: "20px", alignItems: "center", }}>
       <h1>{count}</h1>
 
-      <button onClick={() => dispatch(increment())}>
-        + by 1
-      </button>
+      
 
-      <button onClick={() => dispatch(decrement())}>
-        - by 1
-      </button>
+        <button  onClick={() => dispatch(increment())}>
+          + by 1
+        </button>
 
-      <button onClick={() => dispatch(incrementby5())}>
-        + by 5
-      </button>
+        <button onClick={() => dispatch(decrement())}>
+          - by 1
+        </button>
+
+        <button onClick={() => dispatch(incrementby5())}>
+          + by 5
+        </button>
     </div>
   )
 }
